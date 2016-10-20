1c08151cd57bf7cde58b1fd9df6c0d6e56e71642
zhangkai <zkxw2008@hotmial.com>
2016-10-20 18:27:22 +0800

    code commit

---
+++ app/routes.js
@@ -0,0 +1,28 @@
+module.exports = (app, passport) => {
+  app.get('/', (rq, res)=>{
+    res.render('index.ejs')
+  })
+
+  app.get('/login', (req, res) => {
+    res.render('login.ejs', { message : req.flash('loginMessage') })
+  })
+
+  app.get('/signup', (req, res) => {
+    res.render('signup.ejs', { message : req.flash('signupMessage') })
+  })
+
+  app.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile.ejs', { user : req.user })
+  })
+
+  app.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+  })
+}
+
+var isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated())
+    return next()
+  res.redirect('/')
+}
